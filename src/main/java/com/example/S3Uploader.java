72f0d5048e8ce49e83bea841dabab247997e4b87
Yohei Ueki <Yohei.Ueki@sony.com>
2025-05-23 17:09:27 +0900

    Migrate to AWS SDK v2: update code, tests, configuration, and JDK compatibility

--- src/main/java/com/example/S3Uploader.java
+++ src/main/java/com/example/S3Uploader.java
@@ -1,14 +1,19 @@
 package com.example;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
 
 public class S3Uploader {
     public static void main(String[] args) throws IOException {
@@ -26,21 +31,34 @@ public class S3Uploader {
             writer.write("This is a dummy file for S3 upload sample.");
         }
 
-        // S3クライアント作成
-        AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
-                .withPathStyleAccessEnabled(true)
+        // S3クライアント作成（AWS SDK v2）
+        S3Client s3 = S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)
+                        .build())
                 .build();
 
         // バケット作成（存在しない場合）
-        if (!s3.doesBucketExistV2(bucketName)) {
-            s3.createBucket(bucketName);
+        ListBucketsResponse listBucketsResponse = s3.listBuckets();
+        boolean exists = listBucketsResponse.buckets().stream()
+                .anyMatch(b -> b.name().equals(bucketName));
+        if (!exists) {
+            s3.createBucket(CreateBucketRequest.builder()
+                    .bucket(bucketName)
+                    .build());
             System.out.println("Bucket created: " + bucketName);
         }
 
         // ファイルアップロード
-        s3.putObject(bucketName, key, dummyFile);
+        s3.putObject(PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(key)
+                        .build(),
+                RequestBody.fromFile(dummyFile));
         System.out.println("File uploaded: " + key);
 
         // クリーンアップ
